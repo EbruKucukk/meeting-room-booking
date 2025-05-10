1759cf2fafb55cb5d6558bcd3dd7be273258b9b0
Ebru Küçük <kucukkebru@gmail.com>
2025-05-10 21:19:38 +0300

    mrb-firstversion

---
+++ wwwroot/js/register.js
@@ -0,0 +1,7 @@
+﻿<script>
+    document.querySelector('a[href="/Kayit"]').addEventListener('click', function (e) {
+        e.preventDefault(); // Sayfa yönlenmesin
+    document.querySelector('.container').style.display = 'none'; // Ana içerik gizlenir
+    document.getElementById('register-section').classList.remove('hidden'); // Kayıt formu gösterilir
+    });
+</script>
